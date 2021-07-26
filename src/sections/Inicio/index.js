bedf4fa5e893d4f7957e0f916a37b48d8d52856e
guilhermeg2k <guilhermen2k@gmail.com>
2021-07-26 09:41:49 -0300

    change: fixed github link

--- src/sections/Inicio/index.js
+++ src/sections/Inicio/index.js
@@ -5,13 +5,13 @@ export default function Inicio(){
     <>
       <div className="slide inicio">
         <div className="slideWrapper center">
-          <h1>Portfólio Fisica para Computação </h1>
-          <h3>Esse Portfólio tem como objetivo apresentar e resumir os principais conceitos relacionados aos conteudos apresentados na máteria</h3>
+          <h1>Portfólio Física para Computação </h1>
+          <h3>Esse Portfólio tem como objetivo apresentar e resumir os principais conceitos relacionados aos conteudos apresentados na matéria</h3>
           <h3>Matéria ministrada pela professora Andiara Cardoso</h3>
           <h3>Trabalho realizado pelo aluno Guilherme Vieira Nascimento</h3>
           <p>É possivel navegar pelo Portfólio pelo menu no topo da tela, e pelas setas nas laterias esquerda e direita (que apareram caso a página possua mais contéudo a ser explorado, como por exemplo na página de campos elétricos.).</p>
           <p>Os conceitos aqui apresentados foram retirados do livro Fundamentos de Física - Eletromagnetismo dos autores Halliday e Resnick.</p>
-          <p>O código dessa página é opensource e encontra-se disponível em <a href="https://github.com/guilhermeg2k/Portfólio-fisica">github.com/guilhermeg2k/portifolio-fisica</a>.</p>
+          <p>O código dessa página é opensource e encontra-se disponível em <a href="https://github.com/guilhermeg2k/portfolio-fisica">github.com/guilhermeg2k/portifolio-fisica</a>.</p>
           </div>
       </div>
     </>
